41fe26a61f38e11b3a489e6b0249419150b90139
MrCabbageStick <mrcabbagestick@gmail.com>
2024-02-06 01:39:20 +0100

    Initial setup

---
+++ src/main/java/mrcabbagestick/electrify/ElectrifyDataGenerator.java
@@ -0,0 +1,11 @@
+package mrcabbagestick.electrify;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ElectrifyDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
