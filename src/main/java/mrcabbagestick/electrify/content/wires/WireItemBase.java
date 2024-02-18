13164e9811fa942fa5a3f5fa4fd59f70793e549f
MrCabbageStick <mrcabbagestick@gmail.com>
2024-02-18 14:12:18 +0100

    Separate functions for starting, canceling and ending wires

--- src/main/java/mrcabbagestick/electrify/content/wires/WireItemBase.java
+++ src/main/java/mrcabbagestick/electrify/content/wires/WireItemBase.java
@@ -2,9 +2,7 @@ package mrcabbagestick.electrify.content.wires;
 
 import mrcabbagestick.electrify.Electrify;
 import mrcabbagestick.electrify.content.misc.ElectrifyTextStyles;
-import mrcabbagestick.electrify.content.wire_connectors.WireConnectorBase;
 import mrcabbagestick.electrify.content.wire_connectors.WireConnectorBlockEntity;
-import mrcabbagestick.electrify.tools.ClientGuiTools;
 import mrcabbagestick.electrify.tools.NbtTools;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
@@ -17,6 +15,8 @@ import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
@@ -26,47 +26,72 @@ public abstract class WireItemBase extends Item {
     }
 
     @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        ItemStack usedItem = user.getStackInHand(hand);
+
+        Electrify.LOGGER.info("Wire used");
+
+        if(user.isSneaking() && getTarget(usedItem) != null){
+            Electrify.LOGGER.info("Should cancel");
+            cancelWire(usedItem, user, user.getBlockPos(), world);
+            return TypedActionResult.success(usedItem);
+        }
+        return TypedActionResult.pass(usedItem);
+    }
+
+    @Override
     public ActionResult useOnBlock(ItemUsageContext context) {
         BlockPos clickedPos = context.getBlockPos();
         World world = context.getWorld();
         BlockEntity clickedEntity = world.getBlockEntity(clickedPos);
 
         PlayerEntity player = context.getPlayer();
-        ItemStack usedWith = player.getStackInHand(context.getHand());
+        ItemStack usedItem = player.getStackInHand(context.getHand());
 
         if(clickedEntity instanceof WireConnectorBlockEntity){
 
-            BlockPos currentTarget = getTarget(usedWith);
+            BlockPos currentTarget = getTarget(usedItem);
 
             if(currentTarget == null){
                 // No target selected
-                setTarget(usedWith, clickedPos);
-                player.sendMessage(Text.of("Wire started").getWithStyle(ElectrifyTextStyles.COLOR_SUCCESS).get(0), true);
-                world.playSound(null, clickedPos, SoundEvents.ENTITY_LEASH_KNOT_PLACE, SoundCategory.BLOCKS, 1f, 1f);
+                startWire(usedItem, player, clickedPos, world);
                 return ActionResult.SUCCESS;
             }
 
             if(currentTarget.getSquaredDistance(clickedPos) == 0){
                 // Target is the same as clicked
-                removeTarget(usedWith);
-                player.sendMessage(Text.of("Wire cancelled").getWithStyle(ElectrifyTextStyles.COLOR_FAIL).get(0), true);
-                world.playSound(null, clickedPos, SoundEvents.ENTITY_LEASH_KNOT_BREAK, SoundCategory.BLOCKS, 1f, 1f);
+                cancelWire(usedItem, player, clickedPos, world);
                 return ActionResult.SUCCESS;
             }
 
             if(world.getBlockEntity(currentTarget) instanceof WireConnectorBlockEntity targetBlockEntity) {
                 // Wire can be placed
-                removeTarget(usedWith);
-                player.sendMessage(Text.of("Wire placed").getWithStyle(ElectrifyTextStyles.COLOR_SUCCESS).get(0), true);
-                world.playSound(null, clickedPos, SoundEvents.ENTITY_LEASH_KNOT_PLACE, SoundCategory.BLOCKS, 1f, 1f);
-
-                targetBlockEntity.connectTo(clickedPos, world.getBlockState(currentTarget));
-
+                finishWire(usedItem, player, clickedPos, world, targetBlockEntity, currentTarget);
                 return ActionResult.SUCCESS;
             }
         }
 
         return ActionResult.FAIL;
+    }
+
+    private void cancelWire(ItemStack usedItem, PlayerEntity player, BlockPos clickedPos, World world){
+        removeTarget(usedItem);
+        player.sendMessage(Text.of("Wire cancelled").getWithStyle(ElectrifyTextStyles.COLOR_FAIL).get(0), true);
+        world.playSound(null, clickedPos, SoundEvents.ENTITY_LEASH_KNOT_BREAK, SoundCategory.BLOCKS, 1f, 1f);
+    }
+
+    private void startWire(ItemStack usedItem, PlayerEntity player, BlockPos clickedPos, World world){
+        setTarget(usedItem, clickedPos);
+        player.sendMessage(Text.of("Wire started").getWithStyle(ElectrifyTextStyles.COLOR_SUCCESS).get(0), true);
+        world.playSound(null, clickedPos, SoundEvents.ENTITY_LEASH_KNOT_PLACE, SoundCategory.BLOCKS, 1f, 1f);
+    }
+
+    private void finishWire(ItemStack usedItem, PlayerEntity player, BlockPos clickedPos, World world, WireConnectorBlockEntity target, BlockPos targetPos){
+        removeTarget(usedItem);
+        player.sendMessage(Text.of("Wire placed").getWithStyle(ElectrifyTextStyles.COLOR_SUCCESS).get(0), true);
+        world.playSound(null, clickedPos, SoundEvents.ENTITY_LEASH_KNOT_PLACE, SoundCategory.BLOCKS, 1f, 1f);
+
+        target.connectTo(clickedPos, world.getBlockState(targetPos));
     }
 
     public static BlockPos getTarget(ItemStack item){
